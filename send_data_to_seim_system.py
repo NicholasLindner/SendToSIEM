3d1fb1d74246a93019f66b29509381f8707d1d47
NicholasLindner <nicholaslindner788@gmail.com>
2024-10-14 18:00:49 -0400

    Refractored code

--- send_data_to_seim_system.py
+++ send_data_to_seim_system.py
@@ -2,38 +2,54 @@ import requests
 import json
 
 
-def send_data(url, headers, payload):
+class SEIMSender:
+    def __init__(self, url, token):
+        self.url = url
+        self.token = token
 
-    response = requests.post(url, headers=headers, data=json.dumps(payload), verify=False)
-    if response.status_code == 200:
-        print("Success")
-    else:
-        print(f"Failure status: {response.status_code}")
+    def build_headers(self):
+        raise NotImplementedError("Must be implemented in a subclass.")
+
+    def send_data(self, payload):
+        headers = self.build_headers()
+        response = requests.post(self.url, headers=headers, data=json.dumps(payload), verify=False)
+        if response.status_code == 200:
+            print("Success")
+        else:
+            print(f"Failure status: {response.status_code}")
 
 
-def build_splunk_headers(token):
-    return {
-        "Authorization": f"Splunk {token}",
-        "Content-Type": "application/json"
-    }
+class Splunk(SEIMSender):
 
+    def build_headers(self):
+        return {
+            "Authorization": f"Splunk {self.token}",
+            "Content-Type": "application/json"
+        }
 
-def build_splunk_payload(json_data):
-    return {
-        "event": json_data
-    }
+    def build_payload(self, json_data):
+        return {
+            "event": json_data
+        }
 
 
 def main():
     try:
-        data = input("JSON Data: ")
-        url = input("Destination URL: ")
-        token = input("Authorization Token: ")
+        seim_choice = input('Enter "Splunk" if you are using Splunk, otherwise please enter "Other": ')
+        if seim_choice == "Splunk":
 
-        headers = build_splunk_headers(token)
-        payload = build_splunk_payload(json.loads(data))
+            data = input("JSON Data: ")
+            url = input("Destination URL: ")
+            token = input("Authorization Token: ")
 
-        send_data(url, headers, payload)
+            sender = Splunk(url, token)
+            payload = sender.build_payload(json.loads(data))
+            sender.send_data(payload)
+        else:
+            print("Only Splunk is supported currently.")
+
+    except json.JSONDecodeError:
+        print("Please provide valid JSON data next time.")
     except Exception as error:
         print(f"Error: {error}")
 
